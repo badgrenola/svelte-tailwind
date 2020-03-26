57805666ba827d8ebe2a7f1348e09d5a8e6ebd2c
Matt Brealey <matt.brealey@filigree.tech>
2020-03-26 17:43:50 +0000

    Initial commit

---
+++ postcss.config.js
@@ -0,0 +1,20 @@
+const production = !process.env.ROLLUP_WATCH;
+const purgecss = require("@fullhuman/postcss-purgecss");
+const cssnano = require('cssnano');
+
+module.exports = {
+    plugins: [
+        require("tailwindcss"),
+        require("autoprefixer"),
+        cssnano({
+         autoprefixer: false,
+         preset: ['default'],
+	    }),
+		// Only purge css on production
+        production &&
+            purgecss({
+                content: ["./**/*.html", "./src/**/*.svelte"],
+                defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g || [])
+            })
+    ]
+}
---
+++ src/App.svelte
@@ -0,0 +1,5 @@
+<main class="w-full h-full">
+	<div class="w-full h-full flex justify-center items-center">
+		<h1 class="text-3xl text-gray-900">SvelteJS + TailwindCSS = 👍</h1>
+	</div>
+</main>
